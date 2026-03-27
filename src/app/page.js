fe560cc4421470641886320d4ef49e2595f8c305
nfaiyaz <faiyaz.noor@outlook.com>
2026-03-27 18:01:53 +0600

    Adding product section to the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import NewProducts from "@/components/NewProducts";
 
 export default function Home() {
   return (
     <main>
-      <Hero/>
+      <Hero />
+      <NewProducts/>
     </main>
   );
 }
